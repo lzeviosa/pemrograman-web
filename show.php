5e59d4bb47e8e9e631b2a9fb8efa2b2eaa7844a9
Delvita <delvitawidaningsih11@gmail.com>
2023-11-19 17:22:04 +0700

    Initial Commit

--- show.php
+++ show.php
@@ -3,44 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Produk</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
-<body>
-    <h1>Data produk</h1>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nama produk</th>
-                <th>harga</th>
-                <th>gambar produk</th>
-                <th>Opsi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                require './config/db.php';
+<body style="background: rgb(200, 200, 200);">
 
-                $products = mysqli_query($db_connect,"SELECT * FROM products");
-                $no = 1;
+    <div class="container bg-white position-absolute top-50 start-50 translate-middle shadow p-5 rounded-2" style="max-width: 600px;">
 
-                while($row = mysqli_fetch_assoc($products)) {
-            ?>
+        <h1 class="text-center">Data produk</h1>
+
+        <table class="table mt-5">
+            <thead>
                 <tr>
-                    <td><?=$no++;?></td>
-                    <td><?=$row['name'];?></td>
-                    <td><?=$row['price'];?></td>
-                    <!-- <td><img src="<?=$row['image'];?>" width="100"></td> -->
-                    <td><a href="<?=$row['image'];?>" target="_blank">unduh</a></td>
-                    <td>
-                        <a href="edit.php?id=<?=$row['id'];?>">Edit</a>
-                        <a href="delete.php?id=<?=$row['id'];?>">Hapus</a>
-
-                    </td>
+                    <th>#</th>
+                    <th>Nama produk</th>
+                    <th>harga</th>
+                    <th>gambar produk</th>
+                    <th>Opsi</th>
                 </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php
+                    require './config/db.php';
+
+                    $products = mysqli_query($db_connect,"SELECT * FROM products");
+                    $no = 1;
+
+                    while($row = mysqli_fetch_assoc($products)) {
+                ?>
+                    <tr>
+                        <td><?=$no++;?></td>
+                        <td><?=$row['name'];?></td>
+                        <td><?=$row['price'];?></td>
+                        <!-- <td><img src="<?=$row['image'];?>" width="100"></td> -->
+                        <td><a href="<?=$row['image'];?>" target="_blank">unduh</a></td>
+                        <td>
+                            <a href="edit.php?id=<?=$row['id'];?>">Edit</a>
+                            <a href="delete.php?id=<?=$row['id'];?>">Hapus</a>
+
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+
+    </div>
     
 </body>
 </html>
